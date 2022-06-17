6f9ee257b3d93d0aeffed5cf5e364a50cc24faf3
ramadevi <kotagiriramadeviss661@gmail.com>
2022-06-17 14:35:30 +0530

    third commit

--- firstdata/src/firstdata/array.java
+++ firstdata/src/firstdata/array.java
@@ -3,7 +3,12 @@ package firstdata;
 public class array {
 
 	public static void main(String[] args) {
-int[] x=10;
+int[] x=new int [2];
+ x[0]=1;
+ x[1]=2;
+ for(int i=0;i<x.length;i++)
+	 
+ System.out.println(x[i]);
 
 	}
 
