17338c9947fc1941adf5bfe4027e23634d36bcc3
ramadevi661 <60340236+ramadevi661@users.noreply.github.com>
2022-06-17 14:42:06 +0530

    Update array.java

--- firstdata/src/firstdata/array.java
+++ firstdata/src/firstdata/array.java
@@ -3,9 +3,10 @@ package firstdata;
 public class array {
 
 	public static void main(String[] args) {
-int[] x=new int [2];
+int[] x=new int [3];
  x[0]=1;
  x[1]=2;
+ x[2]=3;		
  for(int i=0;i<x.length;i++)
 	 
  System.out.println(x[i]);
